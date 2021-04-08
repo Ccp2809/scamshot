b6f4aecc0423cae8a6a01abc5223cf78687330df
Ccp2809 <52442383+Ccp2809@users.noreply.github.com>
2021-04-08 22:06:21 +0200

    Shotscam v2
    This new version allows you to iterate values not only at the end of the URL, but anywhere you want.

--- shotscam.py
+++ shotscam.py
@@ -6,9 +6,19 @@ from datetime import datetime
 import time
 import argparse
 
+print("""   
+  _________.__            __   _________                      
+ /   _____/|  |__   _____/  |_/   _____/ ____ _____    _____  
+ \_____  \ |  |  \ /  _ \   __\_____  \_/ ___\ __  \  /     \ 
+ /        \|   Y  (  <_> )  | /        \  \___/ __ \|  Y Y  \ 
+/_______  /|___|  /\____/|__|/_______  /\___  >____  /__|_|  /
+        \/      \/                   \/     \/     \/      \/
+        """)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-V", "--version", help="show program version", action="store_true")
 parser.add_argument("--example", "-e", help="show a brief explanation", action="store_true")
+parser.add_argument("--extension", "-ext", action="store", dest="ext", help="set the extension", type=str, required=False)
 requiredArguments = parser.add_argument_group("Required arguments")
 requiredArguments.add_argument("--keyword", "-k", help="set keyword to look for in the URL source code", type=str)
 requiredArguments.add_argument("--width", "-w", help="set output width", type=int)
@@ -19,9 +29,9 @@ requiredArguments.add_argument("--url", "-u", help="set target URL", type=str)
 args = parser.parse_args()  # Reads arguments from the command line
 
 if args.version:
-    print("v1")
+    print("v2")
 elif args.example:
-    print("For URL 'http://example.com/98698' you must input the URL value 'http://example.com/' enclosed within double quotes. The sequence (s) in this case would be 99999.")
+    print("For URL 'http://example.com/98698' you must input the URL value 'http://example.com/'. The sequence (s) in this case would be 99999.")
 elif args.width:
     screen_width = "%i" % args.width
 elif args.height:
@@ -30,33 +40,57 @@ elif args.sequence:
     sequence = "%i" % args.sequence
 elif args.url:
     url = "%s" % args.url
+elif args.ext:
+    ext = "%s" % args.ext
 else:
     print("Invalid argument. Use --help or -h to see the valid options")
     exit(2)
 
+#base_url = 'https://www.vouchersaddict.com/cgi-bin/wingame.pl?source_pk=5617&partner_pk=312&wingame_pk=66&freetest_pk='
 current_date = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 screenshot_extension = ".png"
 
-for number in range(1, args.sequence+1):
-    url = args.url+str(number)
-    status = requests.get(url).status_code
 
+class Colors:
+    OK_CYAN = "\033[96m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    Color_Off = "\033[0m"
+
+
+def url_check():
     if status == 404:
         print(url, "is not up, returned ", status)
     elif status == 200:
         print(url, "is up, returned ", status)
         source_code = requests.get(url).text
         if args.keyword in source_code:
-            print(url, "is a match!")
+            print(Colors.OK_CYAN + url, "is a match!")
             webbrowser.open(url)
-            print("Taking screenshot...")
+            print("Taking screenshot..." + Colors.Color_Off)
             time.sleep(5)
             screenshot = pyscreenshot.grab(bbox=(0, 35, args.width, args.height))
             screenshot.save(current_date + screenshot_extension)
         else:
-            print(url, "is not a match.")
+            print(Colors.RED + url, "is not a match." + Colors.Color_Off)
 
     elif status == 301 or 302:
-        print(url, "is a redirecting URL, returned ", status)
+        print(Colors.YELLOW + url, "is a redirecting URL." + Colors.Color_Off)
     else:
         print(url, "returned ", status)
+
+
+number = 290
+
+while number <= args.sequence:
+
+    if args.ext == "":
+        url = args.url+str(number)
+        status = requests.get(url).status_code
+        number = number+1
+        url_check()
+    else:
+        url = args.url+str(number)+args.ext
+        status = requests.get(url).status_code
+        number = number+1
+        url_check()
